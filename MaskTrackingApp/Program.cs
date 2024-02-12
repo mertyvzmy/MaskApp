bb40121e3597a4cdc7476b31d5ff24d8752ff748
mertyvzmy <112662449+mertyvzmy@users.noreply.github.com>
2024-02-13 02:13:30 +0300

    Add project files.

---
+++ MaskTrackingApp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+
+
+SelamVer(isim: "Ali");
+
+
+static void SelamVer(string isim = "noname")
+{
+    Console.WriteLine("Merhaba ");
+}
+
+
+Person person1 = new Person();
+person1.FirstName = "İrfan Mert";
+person1.LastName = "Yavuz";
+person1.BirtYear = 1998;
+person1.NatinonalIdentity = 48115364686 ; 
+
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+public class citizen
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public int BirthYear { get; set; }
+    public long CitizenNo { get; set; }
+}
